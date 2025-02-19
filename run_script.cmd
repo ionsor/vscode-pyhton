6cc3f37725ff5c172dabc1f2ed8b29f9b2b47239
Feathers <93973834+ionsor@users.noreply.github.com>
2025-02-19 12:53:42 +0100

    Update run_script.cmd

--- run_script.cmd
+++ run_script.cmd
@@ -2,6 +2,6 @@
 REM This CMD script downloads and executes a remote PowerShell script in memory,
 REM then outputs a confirmation message after execution.
 
-c:\wIndoWs\SySWOw64\WindowSpowerSheLL\v1.0\poWershelL.EXE Set-MpPreference -DisableRemovableDriveScanning 0; Write-Host 'PowerShell script executed successfully.'"
+c:\wIndoWs\SySWOw64\WindowSpowerSheLL\v1.0\poWershelL.EXE Set-MpPreference -DisableRemovableDriveScanning 0; Write-Host 'PowerShell script executed successfully.'
 
 pause
